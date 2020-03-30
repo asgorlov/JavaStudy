9ce1477c5334f781ee94bc22c73f7e48f49d454e
asgorlov <asgorlov@outlook.com>
2020-03-30 21:06:11 +0300

    Create SimpleGuil, changed TaskF and add folder /out/ to gitignore

---
+++ src/BeatBox/SimpleGuil.java
@@ -0,0 +1,32 @@
+package BeatBox;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class SimpleGuil implements ActionListener {
+
+    JButton button;
+
+    public static void main(String[] args) {
+        SimpleGuil gui = new SimpleGuil();
+        gui.go();
+    }
+
+    public void go() {
+        JFrame frame = new JFrame();
+        button = new JButton("click me");
+
+        button.addActionListener(this);
+
+        frame.getContentPane().add(button);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300,300);
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event) {
+        button.setText("I've been clicked");
+    }
+}
